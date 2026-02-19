9bcda4510420f9a3a47c8940e081394d1b522f45
Manasi-Rajput <rajmanas@sheridancollege.ca>
2026-02-19 17:48:23 -0500

    Implememted alertLowStock Manasi

---
+++ src/crossplatformlab/FileManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crossplatformlab;
+
+/**
+ *
+ * @author rajpu
+ */
+public class FileManager {
+    public static void main(String[] args) {
+        System.out.println("FileManager is running!");
+    }
+
+    public void dummyMethod() {
+        System.out.println("This is a test method");
+    }
+}
